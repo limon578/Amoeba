b19091d0bf1035772e85b34f1e9481cb7024ca42
limon578 <115075168+limon578@users.noreply.github.com>
2022-12-05 02:25:29 +0300

    Update main.py
    Добавлены уровни сложности

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 from game import Game
 from menu import menu
 from info import info
+from level import level
 
 if __name__ == "__main__":
     pygame.init()
@@ -14,11 +15,30 @@ if __name__ == "__main__":
         if t == 100:
             t = info(g)
 
+        if t == 300:
+            t = level(g)
+
+        if t == 350:
+            t = menu(g)
+
+        if t == 30:
+            t = g.game(30)
+            t = t and menu(g)
+
+        if t == 80:
+            t = g.game(80)
+            t = t and menu(g)
+
+        if t == 150:
+            t = g.game(150)
+            t = t and menu(g)
+        
+
         if t == 200:
             t = menu(g)
 
         if t < 100:
-            t = g.game(t)
+            t = g.game(140)
             t = t and menu(g)
 
     pygame.quit()
