da060b8ee26ec4cea792d4c2b2952c5f21337de0
limon578 <115075168+limon578@users.noreply.github.com>
2022-12-07 13:58:31 +0300

    Update main.py
    Комментарии к переменной t

--- main.py
+++ main.py
@@ -12,29 +12,38 @@ if __name__ == "__main__":
     t = menu(g)
     
     while t is not False:
+        
         if t == 3:
+            # Открытие главного меню
             t = menu(g)
 
         if t == 1:
+            # Открытие меню info
             t = info(g)
 
         if t == 2:
+            # Открытие меню level
             t = level(g)
-
+      
         if t == 30:
+            # Уровень сложности Hard
             t = g.game(30)
             t = t and menu(g)
-       
+        
         if t == 80:
+            # Уровень сложности Medium
             t = g.game(80)
             t = t and menu(g)
-
+         
         if t == 150:
+            # Уровень сложности Easy
             t = g.game(150)
             t = t and menu(g)
-
+         
         if t == 140:
+            # Уровень сложности Play (обычынй)
             t = g.game(140)
             t = t and menu(g)
 
     pygame.quit()
+
